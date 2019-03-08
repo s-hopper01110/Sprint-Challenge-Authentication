05409142bcee970f0fc311a861bd88f87e8ecf82
Sheila <s.hopper01110@gmail.com>
2019-03-08 12:26:59 -0500

    /login works

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcryptjs');
 const { authenticate } = require('../auth/authenticate');
 const Users = require('../config/users-model.js')
 
+const secret = process.env.JWT_SECRET ||'where does the rainbow end?' 
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -33,10 +35,53 @@ function register(req, res) {
 
 
 
+
+
+
+
 //LOGIN:implement user login
-function login(req, res) {
-  
+function generateToken(user) { 
+  const payload = {
+      subject: user.id, // sub in payload is what the token is about 
+      username: user.username,
+      // .... secret stays out of this function 
+  }
+
+  const options = {
+      expiresIn: '1d'
+  }
+ 
+  return jwt.sign(payload, secret, options)
 }
+
+//------------------------------------------------
+
+function login(req, res) {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+
+      // check that passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user) // produces  a token 
+        //return token 
+        res
+          .status(200)
+          .json({ message: `Welcome ${user.username}!, here's your token...`, token, }); //(token implemented sends token to the client)
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+};
+
+
+
+
 
 
 //GET for jokes:
