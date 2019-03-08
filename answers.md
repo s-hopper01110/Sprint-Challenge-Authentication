68a0d8c896e5daa27a47155bbd314fcb6cf13f34
Sheila <s.hopper01110@gmail.com>
2019-03-08 12:02:37 -0500

    /register working

--- answers.md
+++ answers.md
@@ -1,4 +1,4 @@
 What is the purpose of using sessions?
 What does bcrypt do to help us store passwords in a secure manner.
 What does bcrypt do to slow down attackers?
-What are the three parts of the JSON Web Token?
+What are the three parts of the JSON Web Token? Headers,Payload,Signature.
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 const { authenticate } = require('../auth/authenticate');
+const Users = require('../config/users-model.js')
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,14 +13,33 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+
+//REGISTER: implement user registration
 function register(req, res) {
-  // implement user registration
+
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // generates hash for user's password
+  user.password = hash;//overrides user.password w/ hash
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  
 }
 
+
+
+//LOGIN:implement user login
 function login(req, res) {
-  // implement user login
+  
 }
 
+
+//GET for jokes:
 function getJokes(req, res) {
   const requestOptions = {
     headers: { accept: 'application/json' },
